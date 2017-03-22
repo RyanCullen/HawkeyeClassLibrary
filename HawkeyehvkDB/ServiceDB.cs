ada4555f34d90ce82815a9635f62ccdfd5306eb1
1530981 <Brae@B221-23>
2017-03-22 12:44:21 -0400

    adding code to Service DB

--- HawkeyehvkDB/ServiceDB.cs
+++ HawkeyehvkDB/ServiceDB.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Configuration;
+using Oracle.ManagedDataAccess.Client;
 
 namespace HawkeyehvkDB
 {
     class ServiceDB
     {
+        public DataSet listServices(long petRes) {
+            string conString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+            OracleConnection con = new OracleConnection(conString);
+            string cmdStr = "";
+            OracleCommand cmd = new OracleCommand(cmdStr, con);
+
+            OracleDataAdapter da = new OracleDataAdapter(cmd);
+            da.SelectCommand = cmd;
+
+            DataSet ds = new DataSet("ServiceDataSet");
+            da.Fill(ds, "hvk_Service");
+            return ds;
+        }
     }
 }
