90a2d0b6dcdfaec358dfa9c62da660661358a3ad
Ryan Cullen <ryan.cullen@live.ca>
2017-03-23 11:56:13 -0400

    Added owner_number to listOwners select statement

--- HawkeyehvkDB/OwnerDB.cs
+++ HawkeyehvkDB/OwnerDB.cs
@@ -15,7 +15,7 @@ namespace HawkeyehvkDB
         {
             string conString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
             OracleConnection con = new OracleConnection(conString);
-            string cmdStr = "SELECT OWNER_LAST_NAME ||', '||OWNER_FIRST_NAME FROM HVK_OWNER ORDER BY OWNER_LAST_NAME";
+            string cmdStr = "SELECT OWNER_NUMBER, OWNER_LAST_NAME ||', '||OWNER_FIRST_NAME \"OWNER_NAME\" FROM HVK_OWNER ORDER BY OWNER_LAST_NAME";
             OracleCommand cmd = new OracleCommand(cmdStr, con);
 
             OracleDataAdapter da = new OracleDataAdapter(cmd);
@@ -30,7 +30,7 @@ namespace HawkeyehvkDB
         {
             string conString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
             OracleConnection con = new OracleConnection(conString);
-            string cmdStr = "SELECT OWNER_LAST_NAME ||', '||OWNER_FIRST_NAME FROM HVK_OWNER WHERE OWNER_NUMBER = :OWNER_NUMBER ORDER BY OWNER_LAST_NAME";
+            string cmdStr = "SELECT OWNER_NUMBER, OWNER_LAST_NAME ||', '||OWNER_FIRST_NAME \"OWNER_NAME\" FROM HVK_OWNER WHERE OWNER_NUMBER = :OWNER_NUMBER ORDER BY OWNER_LAST_NAME";
             OracleCommand cmd = new OracleCommand(cmdStr, con);
             cmd.Parameters.Add("OWNER_NUMBER", ownerNum);
             OracleDataAdapter da = new OracleDataAdapter(cmd);
