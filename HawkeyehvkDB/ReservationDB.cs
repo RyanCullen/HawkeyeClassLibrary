c705737cbda4e63db11376df6dc0d5b35666895c
1530981 <1530981@B221-14>
2017-03-27 09:52:48 -0400

    Moved DB methods from petReservationDB to reservationDB mrthod names: numberOfRunsAvailable listAvailableRuns

--- HawkeyehvkDB/ReservationDB.cs
+++ HawkeyehvkDB/ReservationDB.cs
@@ -213,6 +213,54 @@ WHERE TEAMHAWKEYE.HVK_RESERVATION.RESERVATION_START_DATE >= :DateParameter";
                 con.Close();
             }
         }
+        public DataSet listAvailableRunsDB(DateTime start, DateTime end)
+        {
+            string conString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+            OracleConnection con = new OracleConnection(conString);
+            string cmdStr = @"SELECT r.run_number, r.run_size FROM hvk_run r 
+              MINUS
+               (SELECT UNIQUE r.run_number,
+                r.run_size
+              FROM hvk_run r
+            JOIN hvk_pet_reservation pr
+                ON pr.run_run_number = r.run_number
+            JOIN HVK_RESERVATION res
+            ON PR.RES_RESERVATION_NUMBER = RES.RESERVATION_NUMBER
+              AND (RES.RESERVATION_START_DATE >= :START_DATE 
+            AND RES.RESERVATION_START_DATE <= :END_DATE)
+            OR (RES.RESERVATION_END_DATE >= :START_DATE 
+            AND RES.RESERVATION_END_DATE <= :END_DATE)
+            OR (RES.RESERVATION_START_DATE <= :START_DATE
+            AND RES.RESERVATION_END_DATE >= :END_DATE)
+             )";
+            OracleCommand cmd = new OracleCommand(cmdStr, con);
+            cmd.Parameters.Add("START_DATE", start);
+            cmd.Parameters.Add("END_DATE", end);
+            OracleDataAdapter da = new OracleDataAdapter(cmd);
+            da.SelectCommand = cmd;
+            DataSet ds = new DataSet("AvailableRuns");
+            da.Fill(ds, "hvk_runsAvail");
+            return ds;
+        }
+        public DataSet numberOfRunsAvailableDB(DateTime start, DateTime end, char size)
+        {
+            string conString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+            OracleConnection con = new OracleConnection(conString);
+            string cmdStr = @"numberOfRunsAvailable
+                            (:START_DATE, -- input date
+                             :END_DATE, -- input date
+                             :RUN_SIZE, -- input size, the input is large or not large becuase it uses the dog size
+                             lv_count);";
+            OracleCommand cmd = new OracleCommand(cmdStr, con);
+            cmd.Parameters.Add("START_DATE", start);
+            cmd.Parameters.Add("END_DATE", end);
+            cmd.Parameters.Add("RUN_SIZE", size);
+            OracleDataAdapter da = new OracleDataAdapter(cmd);
+            da.SelectCommand = cmd;
+            DataSet ds = new DataSet("NumRuns");
+            da.Fill(ds, "hvk_numRuns");
+            return ds;
+        }
 
     }
 }
